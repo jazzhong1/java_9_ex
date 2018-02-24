e6cfad5c2e6b8c71480845ecab9884f21e250e7d
jazzhong1 <jazzhong1@naver.com>
2018-02-24 10:34:30 +0900

    ver1.1

--- src/com/iu/s1/StudentController.java
+++ src/com/iu/s1/StudentController.java
@@ -14,7 +14,8 @@ public class StudentController {
 		sc=new Scanner(System.in);
 		actions=new Action[2];
 //		actions[0]=new StudentAddService();
-//		actions[1]=new StudentServiceView();
+
+//		actions[1]=new StudentServiceView().;
 	}
 	
 	public void start(){
@@ -24,16 +25,17 @@ public class StudentController {
 		Student students=new Student();
 		StudentInput input=new StudentInput();
 		students=input.addStudent(students);
-		new StudentInfo().StudentView(students);
+//		new StudentInfo().StudentView(students);
 		
 		
 		boolean check=true;
 		while(check) {
-			System.out.println("1.학생추가");
-			System.out.println("2.학생정보출력");
-			System.out.println("3.프로그램종료");
 			int num=sc.nextInt();
+			System.out.println("1.학생추가");
 			actions[num-1].excute(); 	// 배열로 바로 접근할수있게
+			System.out.println("2.학생정보출력");
+
+			System.out.println("3.프로그램종료");
 			
 		}
 		
